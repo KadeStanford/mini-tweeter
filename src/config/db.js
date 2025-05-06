e9208006afc24c2662b669a939a03d79308b5735
Aakash Poudel <aakash.poudel.900@gmail.com>
2025-05-06 08:11:52 -0500

    .env, .gitignore files added

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
 
-const MONGO_URI =
-  "mongodb+srv://kade:Stanford24@cmps415.zsbfjv5.mongodb.net/?retryWrites=true&w=majority&appName=CMPS415";
+const MONGO_URI = process.env.MONGO_URI;
 
 class Database {
   constructor() {
